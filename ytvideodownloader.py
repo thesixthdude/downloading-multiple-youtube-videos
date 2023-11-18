f83764cf9a6013659b9feb368ff5d72b941dbad9
varshith <150644095+thesixthdude@users.noreply.github.com>
2023-11-18 15:45:36 +0530

    Update ytvideodownloader.py

--- ytvideodownloader.py
+++ ytvideodownloader.py
@@ -1,20 +1,26 @@
-# from pytube import YouTube
+from pytube import YouTube
 
-# def Download(link):
-#     youtubeObject = YouTube(link)
-#     youtubeObject = youtubeObject.streams.get_highest_resolution()
-#     try:
-#         youtubeObject.download("C:\\Users\\hp\\Downloads\\")
-#     except:
-#         print("An error has occurred")
-#     print("Download successfull")
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download("C:\\Users\\hp\\Downloads\\")
+      #put the path of the folder where you would want the downloaded videos
+    except:
+        print("An error has occurred")
+    print("Download successfull")
 
-# links_list = []
-# int_of_links = int(input("Enter the number of videos to be downloaded: "))
-# for i in range(int_of_links):
-#     input_link = input("Enter the YouTube video URL: ")
-#     links_list.append(input_link)
+#creating an empty list to store the links of the videos
+links_list = []  
+#getting the number of videos to be downloaded
+int_of_links = int(input("Enter the number of videos to be downloaded: "))
 
-# for video in links_list:
-#     Download(video)
+#using for loop to add multiple links to the list
+for i in range(int_of_links):
+    input_link = input("Enter the YouTube video URL: ")
+    links_list.append(input_link)
+  
+#iterating through the list and downloading each video
+for video in links_list:
+    Download(video)
 
