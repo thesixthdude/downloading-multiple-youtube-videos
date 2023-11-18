af69c916d31103e6a6dc2321c47f324a307b39dc
varshith <150644095+thesixthdude@users.noreply.github.com>
2023-11-18 15:35:59 +0530

    Add files via upload

---
+++ ytvideodownloader.py
@@ -0,0 +1,20 @@
+# from pytube import YouTube
+
+# def Download(link):
+#     youtubeObject = YouTube(link)
+#     youtubeObject = youtubeObject.streams.get_highest_resolution()
+#     try:
+#         youtubeObject.download("C:\\Users\\hp\\Downloads\\")
+#     except:
+#         print("An error has occurred")
+#     print("Download successfull")
+
+# links_list = []
+# int_of_links = int(input("Enter the number of videos to be downloaded: "))
+# for i in range(int_of_links):
+#     input_link = input("Enter the YouTube video URL: ")
+#     links_list.append(input_link)
+
+# for video in links_list:
+#     Download(video)
+
